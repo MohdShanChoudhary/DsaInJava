fd95e6e6a7696656bd65c5227c1bc339237b0218
mohdshanchoudhary <mohdshancs23@gmail.com>
2026-07-13 13:40:51 +0530

    reverse a array

--- questions /string/reverseaAstring1.java
+++ questions /string/reverseaAstring1.java
@@ -1,13 +1,35 @@
 
+// THIS CODE IS ONLY FOR PRINT 
 
+// public class reverseaAstring1 {
+//     public static void main(String[] args){
+//         String str = "Hello World";
+//         int size = str.length();
+
+//         for(int i=size-1;i>=0;i--){
+//             System.out.print(str.charAt(i));
+//         }
+    
+//     }
+// }
+
+
+// and this is for reverse a string 
 public class reverseaAstring1 {
     public static void main(String[] args){
-        String str = "Hello World";
-        int size = str.length();
+        System.out.println("working");
+        String shan="my name is mohd shan";
+        String rev="";
+        char  ch;
 
-        for(int i=size-1;i>=0;i--){
-            System.out.print(str.charAt(i));
+
+        for(int i=0;i<shan.length();i++){
+            ch= shan.charAt(i);
+            rev=ch+rev;
+          //  rev=rev+ch;
         }
-    
+        System.out.println(rev);
+
+
     }
 }
